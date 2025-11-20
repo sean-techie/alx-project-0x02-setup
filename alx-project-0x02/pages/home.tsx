275dcf513bf2e6775ab08c13ad89bcddc507756c
Reseanne Rampou <reseanner@gmail.com>
2025-11-20 13:16:08 +0200

    modified pages/home.tsx

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import Card from "@/components/common/Card";
 
 const Home = () => {
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Home Page</h1>
       <p>Welcome to the Home page!</p>
+
+      {/* Example Card components */}
+      <Card title="Card 1" content="This is the content for card number 1." />
+      <Card title="Card 2" content="Here is some different content for card 2." />
+      <Card title="Card 3" content="Another card with unique content." />
     </div>
   );
 };
